310d6e283470d508f94a00078adc24118ce77e5f
Cote Feldsine <cfeld005@wch136-24.cs.ucr.edu>
2021-04-18 13:15:51 -0700

    Completed Part 2

---
+++ turnin/cfeld005_lab4_part2_tests.py
@@ -0,0 +1,77 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'PINA: 0x00 => PORTC: 7, state: wait',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x07)],
+    },
+
+    {'description': 'Upper Bound check => PORTC: 9, state: press1',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 }, 
+    {'inputs': [('PINA',0x01)], 'iterations': 2 }, 
+    {'inputs': [('PINA',0x00)], 'iterations': 2 }, 
+    {'inputs': [('PINA',0x01)], 'iterations': 2 }, ],
+    'expected': [('PORTC',0x09)],
+    },
+
+    {'description': 'Lower Bound check => PORTC: 0, state: press2',
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x00)],
+    },
+
+    {'description': 'Reset check => PORTC: 0, state: reset',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x03)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x00)],
+    },
+
+    {'description': 'Add/Subtract test => PORTC: 6, state: press1',
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x01)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x02)], 'iterations': 2 },
+    {'inputs': [('PINA',0x00)], 'iterations': 2 },
+    {'inputs': [('PINA',0x01)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x06)],
+    },
+]
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+watch = ['state', 'tmpC']
+
+
