2178169482439ef910832d9423420305537f42b0
Cote Feldsine <cfeld005@wch136-24.cs.ucr.edu>
2021-04-18 11:11:22 -0700

    Initial Exercise 1 Setup

--- source/main.c
+++ source/main.c
@@ -14,10 +14,46 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x01;
+
+    unsigned char tmpB = 0x01;
+
+    enum SM1_STATES {SM1_S1, SM1_S2} SM1_STATE;
+    
+    SM1_STATE = SM1_S1;
 
     /* Insert your solution below */
     while (1) {
-
+	switch(SM1_STATE){
+	    case SM1_S1:
+		printf("got here");
+		if (PINA == 0x01){
+		    SM1_STATE = SM1_S2;
+		}
+		break;
+	    case SM1_S2:
+		if (PINA == 0x01){
+		    SM1_STATE = SM1_S1;
+		}
+		break;
+	    default:
+		SM1_STATE = SM1_S1;
+		break;
+	}
+	
+	switch(SM1_STATE){
+	    case SM1_S1:
+		tmpB = 0x01;
+		break;
+	    case SM1_S2:
+		tmpB = 0x02;
+		break;
+	    default:
+		tmpB = 0x00;
+		break;
+	}
+	PORTB = tmpB;
     }
     return 1;
 }
